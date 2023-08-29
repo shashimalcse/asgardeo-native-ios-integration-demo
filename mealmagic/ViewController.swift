0745f2b4ae5394899763496ab177ebc3cd2bbaad
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-08-29 08:09:37 +0530

    Initial Commit

---
+++ mealmagic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mealmagic
+//
+//  Created by Thilina Shashimal Senarath on 2023-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
